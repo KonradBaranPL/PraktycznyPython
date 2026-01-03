03a0142447100aecda6dd6e47f8f7c0bf9decf1b
Konrad Baran <konradbaran95@gmail.com>
2026-01-03 21:15:38 +0100

    Add code with Lesson 13 project

--- M01/M01_exercises.py
+++ M01/M01_exercises.py
@@ -1,9 +1,22 @@
 # L12 ZADANIE 1
 
-def calculate_characters():
-	text = input("Podaj jakiś tekst: ")
-	text_length = len(text)
-	return text_length
+# def calculate_characters():
+# 	text = input("Podaj jakiś tekst: ")
+# 	text_length = len(text)
+# 	return text_length
+#
+# text_length = calculate_characters()
+# print(f"Tekst ma {text_length} znaków")
 
-text_length = calculate_characters()
-print(f"Tekst ma {text_length} znaków")
+
+# L13 ZADANIE - PROJEKT
+
+
+text = input("Wpisz tekst artykułu: ")
+
+words = len(text.split())
+characters = len(text)
+
+avg_word_length = characters / words
+print(avg_word_length)
+print(text.upper())
