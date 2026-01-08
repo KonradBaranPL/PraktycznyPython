86d493487b1c8ec4541a2c92fa5ec0408ccdd4e9
Konrad Baran <konradbaran95@gmail.com>
2026-01-08 20:49:56 +0100

    Add exercise 07, 08.

---
+++ M02/M02E07_ex.py
@@ -0,0 +1,3 @@
+### 🔴 Ćwiczenie
+# Napisz program, który prosi o jeden, pojedynczy znak, a następnie sprawdza, czy jest to litera, czy coś innego. Ponownie, tak jak w jednym z poprzednich ćwiczeń, musisz znaleźć odpowiednią metodę, która sprawdza, czy dany napis to litera.
+
