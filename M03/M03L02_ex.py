35c31e12f5d8583e313febda132052a2a9e0d7f1
Konrad Baran <konradbaran95@gmail.com>
2026-01-15 21:32:36 +0100

    Add code with exercise 02

---
+++ M03/M03L02_ex.py
@@ -0,0 +1,23 @@
+## 🔴 Ćwiczenie
+
+# Stwórz i wydrukuj string, który zawiera:
+# 1. cudzysłów " (np. tekst w "cudzysłowie"), a także
+# 2. apostrof ' (np. I'm happy).
+# W jaki sposób umieścisz te znaki?
+
+string_1 = 'tekst w "cudzysłowie"'
+string_2 = "I'm happy"
+
+print(string_1)
+print(string_2)
+
+
+# rozwiązanie: cudzysłów i apostrofy mają być użyte w jednym stringu
+
+string_3 = "tekst w \"cudzysłowie\" i w 'apostrofie'"
+string_4 = 'tekst w "cudzysłowie" i w \'apostrofie\''
+# string_5 = r"tekst w "cudzysłowie" i w 'apostrofie"
+
+print(string_3)
+print(string_4)
+# print(string_5)
