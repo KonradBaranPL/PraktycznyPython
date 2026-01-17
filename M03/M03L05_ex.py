fc042041dcdf754dc33f73032190184a7ee8548f
Konrad Baran <konradbaran95@gmail.com>
2026-01-17 16:07:33 +0100

    Add empty file with exercise 05 (M02)

---
+++ M03/M03L05_ex.py
@@ -0,0 +1,8 @@
+### 🔴 Ćwiczenie
+
+# 1. Rozwiń program z poprzedniego ćwiczenia tak, aby zapisać zanonimizowany tekst do nowego wyjściowego pliku. 
+# 2. Poproś użytkownika zarówno o nazwę pliku wejściowego (tego, który mamy odczytać, np. plik.txt), jak i wyjściowego (tego, do którego mamy zapisać). 
+# 3. Jeżeli użytkownik nie poda nazwy pliku wyjściowego, wówczas wypisz zanominimizowany tekst funkcją print.
+# 4. Jeżeli plik wyjściowy już istnieje, to go nie nadpisuj. W tym celu trzeba wykorzystać funkcję open w inny sposób. Jak? Znajdź w dokumentacji tej funkcji!
+
+
