dcba77e4c007264cae358700ccf184369e1d721d
Konrad Baran <konradbaran95@gmail.com>
2026-01-23 20:35:04 +0100

    Start of exr. 18/M03

---
+++ M03/M03L18_add_ex.py
@@ -0,0 +1,3 @@
+# Bonusowe minićwiczenia:
+# 1. Napisz program, który wydrukuje prostokąt o wymiarach m x n, składający się z znaku *. Rozmiar prostokąta powinien być określony przez dwie zmienne: m (ilość wierszy) oraz n (ilość kolumn). Podpowiedź: użyj `for i in range(10)`, aby dokonać 10 powtórzeń.
+# 2. Napisz program, który wydrukuje tabliczkę mnożenia dla liczb od 1 do 10. Podpowiedź: sprawdź jakie jeszcze argumenty przyjmuje funkcja range.
